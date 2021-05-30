cc745a493e8940596e776589300d50bf28524a08
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-30 16:33:33 +0300

    Task 05 added

--- 1/src/by/epam/unit04/main/Task05.java
+++ 1/src/by/epam/unit04/main/Task05.java
@@ -1,7 +1,34 @@
 package by.epam.unit04.main;
 
+import java.util.Random;
+
 public class Task05 {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        Random rand = new Random();
 
+        for(int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(1000);
+        }
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.printf("%4d", array[i]);
+        }
+        System.out.println();
+
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while(j >= 0 && temp < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.printf("%4d", array[i]);
+        }
     }
 }
