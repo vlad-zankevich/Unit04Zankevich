cea8b16b6b38c8b57fe2d1346e1219f6f8c4ffa9
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-30 15:45:08 +0300

    Task 04 added

--- 1/src/by/epam/unit04/main/Task04.java
+++ 1/src/by/epam/unit04/main/Task04.java
@@ -1,7 +1,39 @@
 package by.epam.unit04.main;
 
+import java.util.Random;
+
 public class Task04 {
     public static void main(String[] args) {
+        int [] mas = new int[10];
+        Random rand = new Random();
+
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = rand.nextInt(1000);
+        }
+
+        for (int j = 0; j < mas.length; j++) {
+            System.out.printf("%4d", mas[j]);
+        }
+        System.out.println();
+
+        int max, temp;
+        for(int a = 0; a < mas.length - 1; a++) {
+            max = a;
+
+            for (int j = a +1; j < mas.length; j++) {
+                if (mas[j] > mas[max]) {
+                    max = j;
+                }
+            }
+            temp = mas[a];
+            mas[a] = mas[max];
+            mas[max] = temp;
+        }
+
+        for (int i = 0; i < mas.length; i++) {
+            System.out.printf("%4d", mas[i]);
+        }
+        System.out.println();
 
     }
 }
