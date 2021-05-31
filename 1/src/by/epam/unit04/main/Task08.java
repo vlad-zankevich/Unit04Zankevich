693628695c94ee6f71d49031587ef7e6b9b6f233
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-05-31 22:05:41 +0300

    Task 08 added

--- 1/src/by/epam/unit04/main/Task08.java
+++ 1/src/by/epam/unit04/main/Task08.java
@@ -1,7 +1,40 @@
 package by.epam.unit04.main;
 
+import java.util.Random;
+
 public class Task08 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int n = 10;
+        int m = 10;
+        int k = 5;
+        int p = 7;
 
+        int [][] mas = new int[m][n];
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                mas[i][j] = rand.nextInt(999);
+            }
+        }
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                System.out.printf("[%4d]", mas[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int j = 0; j < mas[k].length; j++) {
+            System.out.printf("[%4d]", mas[k][j]);
+        }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < mas.length; i++) {
+            System.out.printf("[%4d]", mas[i][p]);
+            System.out.println();
+        }
     }
 }
