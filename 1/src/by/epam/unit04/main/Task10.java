8e9778e055d2507f730e5912c16eff20e0b557e9
Vlad Zankevich <vlad.zankevich@gmail.com>
2021-06-01 21:59:57 +0300

    Task 10 added

--- 1/src/by/epam/unit04/main/Task10.java
+++ 1/src/by/epam/unit04/main/Task10.java
@@ -1,7 +1,33 @@
 package by.epam.unit04.main;
 
+import java.util.Random;
+
 public class Task10 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        int n = rand.nextInt(10);
+        int m = (n * 2 + 4);
 
+        int [][] mas = new int[m][m];
+
+        for (int j = 0; j < mas[0].length; j++) {
+            mas[0][j] = 1;
+            mas[m - 1][j] = 1;
+        }
+
+        for (int i = 1; i < (mas.length - 1); i++) {
+            mas[i][0] = 1;
+        }
+
+        for (int i = 1; i < (mas.length - 1); i++) {
+            mas[i][m - 1] = 1;
+        }
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                System.out.printf(" %1d ", mas[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
